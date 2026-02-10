f16fc3681592e5120d0a857fbb2a5c672be124c2
GrandMoze <grandmozee@gmail.com>
2026-02-10 12:08:02 +0700

    week01: (tugas 2) implement null safety for user note

--- src/oop_00000110660_grandmoze/week01/GameStore.kt
+++ src/oop_00000110660_grandmoze/week01/GameStore.kt
@@ -2,16 +2,18 @@ package oop_00000110660_grandmoze.week01
 
 fun calculateDiscount(price: Int) = if (price > 500000) price * 20 / 100 else price * 10 / 100
 
-fun printReceipt(title: String, originalPrice: Int, finalPrice: Int) {
+fun printReceipt(title: String, originalPrice: Int, finalPrice: Int, userNote: String?) {
     println("STRUK PEMBAYARAN STEAMKW")
     println("Judul Game : $title")
     println("Harga Asli : Rp ${String.format("%,d", originalPrice)}")
     println("Harga Akhir: Rp ${String.format("%,d", finalPrice)}")
+    println("Catatan    : ${userNote ?: "Tidak ada catatan"}") // Elvis Operator
 }
 
 fun main() {
     val gameTitle = "Cyberpunk 2077"
     val price = 650000
+    val userNote: String? = null // Nullable type sesuai requirement
 
     val discount = calculateDiscount(price)
     val finalPrice = price - discount
@@ -20,6 +22,7 @@ fun main() {
     printReceipt(
         title = gameTitle,
         originalPrice = price,
-        finalPrice = finalPrice
+        finalPrice = finalPrice,
+        userNote = userNote // Named argument untuk nullable parameter
     )
 }
