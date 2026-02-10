00e4973985b430a9df8b49b9c37a7f0930f780b1
GrandMoze <grandmozee@gmail.com>
2026-02-10 11:52:11 +0700

    week01: (tugas 1) clean up unused argos and comments

--- src/oop_00000110660_grandmoze/week01/CircleCalculator.kt
+++ src/oop_00000110660_grandmoze/week01/CircleCalculator.kt
@@ -1,15 +1,10 @@
 package oop_00000110660_grandmoze.week01
 
-fun main(args: Array<String>) {
-    // Variable definition
+fun main() {
     val radius = 7.0
     val pi = 3.14
-    // Calculation
     val area = pi * radius * radius
-    // Output using String Template
     println("Radius: $radius, Area: $area")
-
-    // Logic check using expression body function
     println(checkSize(area))
 }
 
