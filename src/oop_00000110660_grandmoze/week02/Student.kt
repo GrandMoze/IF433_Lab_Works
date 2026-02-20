72b664726570c43e2863335e33bc9fe44e04b795
GrandMoze <grandmozee@gmail.com>
2026-02-20 15:38:46 +0700

    week02: add secondary constructor

--- src/oop_00000110660_grandmoze/week02/Student.kt
+++ src/oop_00000110660_grandmoze/week02/Student.kt
@@ -1,8 +1,15 @@
 package oop_00000110660_grandmoze.week02
 
 class Student (val name: String, val nim: String, var major: String) {
+
+    // Secondary Constructor
+    // Memanggil Primary Constructor dengan nilai default untuk major
+    constructor(name: String, nim: String) : this(name, nim, "Non-Matriculated") {
+        println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
+    }
+
     init {
-        // Validasi Sederhana: Cek panjang NIM
+        // Validasi Sederhana: Cek panjang NIM [cite: 30]
         if (nim.length != 5) {
             println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
             println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
