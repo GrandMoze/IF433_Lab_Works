932c9471166580fdee0b0b04983e6c5df1aa1e46
GrandMoze <grandmozee@gmail.com>
2026-02-24 16:04:10 +0700

    week03: (task 2) create Player class with private xp

---
+++ src/oop_00000110660_grandmoze/week03/Player.kt
@@ -0,0 +1,21 @@
+package oop_00000110660_grandmoze.week03
+
+class Player(val username: String) {
+    private var xp: Int = 0
+
+    val level: Int
+        get() = (xp / 100) + 1
+
+    fun addXp(amount: Int) {
+        if (amount > 0) {
+            val oldLevel = level
+            xp += amount
+
+            if (level > oldLevel) {
+                println("Level Up! Selamat $username naik ke level $level")
+            }
+        } else {
+            println("XP yang ditambahkan harus positif!")
+        }
+    }
+}
