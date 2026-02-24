7253f73e1b687e95103e14968cfea58a397ca39d
GrandMoze <grandmozee@gmail.com>
2026-02-24 15:59:22 +0700

    week03: (task 1) implement Weapon testing in main

--- src/oop_00000110660_grandmoze/week03/Main.kt
+++ src/oop_00000110660_grandmoze/week03/Main.kt
@@ -9,6 +9,21 @@ fun main() {
     println("Gaji saat ini: ${e.salary}")
 
     e.increasePerformance()
-
     println("Pajak yang harus dibayar (10%): ${e.tax}")
+
+    // --- TUGAS MANDIRI 1 (WEAPON) ---
+    println("\n--- Tugas 1: Weapon Testing ---")
+
+    val mySword = Weapon("Excalibur")
+    println("Weapon Name: ${mySword.name}")
+
+    println("Mencoba set damage ke -50...")
+    mySword.damage = -50
+    println("Damage saat ini: ${mySword.damage}")
+
+    println("\nMencoba set damage ke 9999...")
+    mySword.damage = 9999
+    println("Damage saat ini: ${mySword.damage}")
+
+    println("Tier Senjata: ${mySword.tier}")
 }
