abe56b9c943ce74311cf807c869e337454eabbe5
GrandMoze <grandmozee@gmail.com>
2026-02-24 14:58:53 +0700

    week03: simulate stack overflow error in setter

--- src/oop_00000110660_grandmoze/week03/Employee.kt
+++ src/oop_00000110660_grandmoze/week03/Employee.kt
@@ -2,4 +2,8 @@ package oop_00000110660_grandmoze.week03
 
 class Employee(val name:String) {
     val salary:Int = 0
+        set (value) {
+            println("Mencoba set gaji ke : $value")
+            this.salary = value
+        }
 }
