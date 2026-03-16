9fd492d18dbfac1f2c2e396fc84e1f8639076828
GrandMoze <grandmozee@gmail.com>
2026-03-16 12:49:00 +0700

    week06: create Gopay and CreditCard

---
+++ src/oop_00000110660_grandmoze/week06/PaymentImplementations.kt
@@ -0,0 +1,13 @@
+package oop_00000110660_grandmoze.week06
+
+class Gopay : PaymentMethod {
+    override fun pay(amount: Double) {
+        println("Processing Rp$amount via Gopay Server")
+    }
+}
+
+class CreditCard : PaymentMethod {
+    override fun pay(amount: Double) {
+        println("Contacting Bank for Rp$amount")
+    }
+}
