ea10a6ea78c9bc160ab30e14f320e28b741fd6d4
GrandMoze <grandmozee@gmail.com>
2026-04-14 16:25:11 +0700

    week08: test safe casting in main

--- src/oop_00000110660_grandmoze/week08/UserProfile.kt
+++ src/oop_00000110660_grandmoze/week08/UserProfile.kt
@@ -1,5 +1,7 @@
 package oop_00000110660_grandmoze.week08
 
-class UserProfile {
-    val name: String, val email: String?, val phone: String? = null
-}
+class UserProfile(
+    val name: String,
+    val email: String?,
+    val phone: String? = null
+)
