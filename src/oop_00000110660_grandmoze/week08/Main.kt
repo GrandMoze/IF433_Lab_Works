93cb3d5df1f381b5537a0ce680f3f0c6785b71ac
GrandMoze <grandmozee@gmail.com>
2026-04-14 16:03:20 +0700

    week08: implement safe call with let block

--- src/oop_00000110660_grandmoze/week08/Main.kt
+++ src/oop_00000110660_grandmoze/week08/Main.kt
@@ -1,6 +1,12 @@
 package oop_00000110660_grandmoze.week08
 
 fun main() {
+    println("=== TEST SAFE CALLS & ELVIS ===")
+    val emptyOrder = Order(null, null)
+
+    val destination = emptyOrder.deliveryDetails?.address?.city?.name ?: "Kota Tidak Diketahui"
+    println("Tujuan pengiriman: $destination")
+
     println("\n=== TEST LET BLOCK ===")
     val validOrder = Order(null, 250000)
 
