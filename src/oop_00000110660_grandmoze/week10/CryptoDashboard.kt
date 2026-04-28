405d8559fc5a8f4cca3812735dccdad49c9cf8f5
GrandMoze <grandmozee@gmail.com>
2026-04-28 16:33:46 +0700

    week10: (task) print dashboard using forEach on generic list

--- src/oop_00000110660_grandmoze/week10/CryptoDashboard.kt
+++ src/oop_00000110660_grandmoze/week10/CryptoDashboard.kt
@@ -9,4 +9,9 @@ fun main() {
 
     val response = ApiResponse("200 OK", coinRepo.getAll())
 
+    println("Status: ${response.status}")
+    response.data.forEach { coin ->
+        println("Coin: ${coin.name}, Balance: ${coin.balance}")
+    }
+
 }
