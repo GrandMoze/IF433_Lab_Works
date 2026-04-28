8c74c06f407a0bd7ab4c00be53ec3d27f4897ea3
GrandMoze <grandmozee@gmail.com>
2026-04-28 16:32:39 +0700

    week10: (task) populate coin repository

--- src/oop_00000110660_grandmoze/week10/CryptoDashboard.kt
+++ src/oop_00000110660_grandmoze/week10/CryptoDashboard.kt
@@ -2,4 +2,9 @@ package oop_00000110660_grandmoze.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 0.5))
+    coinRepo.add(Coin("ETH", 10.0))
+    coinRepo.add(Coin("USDT", 1000.0))
+    
 }
