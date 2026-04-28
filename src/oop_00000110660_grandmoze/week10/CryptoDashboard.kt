3dd81901e65c347999d0e3a016fc6ca26ac92b90
GrandMoze <grandmozee@gmail.com>
2026-04-28 16:33:12 +0700

    week10: (task) wrap repository data into ApiResponse

--- src/oop_00000110660_grandmoze/week10/CryptoDashboard.kt
+++ src/oop_00000110660_grandmoze/week10/CryptoDashboard.kt
@@ -6,5 +6,7 @@ fun main() {
     coinRepo.add(Coin("BTC", 0.5))
     coinRepo.add(Coin("ETH", 10.0))
     coinRepo.add(Coin("USDT", 1000.0))
-    
+
+    val response = ApiResponse("200 OK", coinRepo.getAll())
+
 }
