ba9aebea900d011c01e485da23d4b1e721e73852
GrandMoze <grandmozee@gmail.com>
2026-05-12 15:22:26 +0700

    week12: create Insufficient Funds exception

---
+++ src/oop_00000110660_grandmoze/week12/CustomExceptions.kt
@@ -0,0 +1,7 @@
+package oop_00000110660_grandmoze.week12
+
+class InsufficientFundsException(
+    val amount: Double,
+    val balance: Double
+) : Exception("Attempted $amount, balance: $balance")
+
