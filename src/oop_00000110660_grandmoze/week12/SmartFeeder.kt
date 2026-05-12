1abb546722ebc7c94ba98e890b93efeaea9c125a
GrandMoze <grandmozee@gmail.com>
2026-05-12 15:43:08 +0700

    week12: (task) implement dispenseKibble function

---
+++ src/oop_00000110660_grandmoze/week12/SmartFeeder.kt
@@ -0,0 +1,4 @@
+package oop_00000110660_grandmoze.week12
+
+fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
+    require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
