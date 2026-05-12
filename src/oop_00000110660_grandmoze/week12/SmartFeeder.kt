56edeb52ef70662c43855697c9e47e3c51c89833
GrandMoze <grandmozee@gmail.com>
2026-05-12 15:43:56 +0700

    week12: (task) implement hardware validation with throw

--- src/oop_00000110660_grandmoze/week12/SmartFeeder.kt
+++ src/oop_00000110660_grandmoze/week12/SmartFeeder.kt
@@ -2,3 +2,8 @@ package oop_00000110660_grandmoze.week12
 
 fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
     require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
+
+    if (isJammed) {
+        throw DispenserJamException()
+    }
+}
