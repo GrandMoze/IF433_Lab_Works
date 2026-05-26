964d80316a5d2b3f1bbdae2baa1b0111a239e41e
GrandMoze <grandmozee@gmail.com>
2026-05-26 15:11:59 +0700

    week14: (task) refactor OrderProcessor applying SRP and DIP

--- src/oop_00000110660_grandmoze/week14/EcommerceTask.kt
+++ src/oop_00000110660_grandmoze/week14/EcommerceTask.kt
@@ -1,5 +1,5 @@
 package oop_00000110660_grandmoze.week14
-
+/*
 import java.io.File
 
 class BadOrderProcessor {
@@ -21,5 +21,55 @@ class BadOrderProcessor {
 
         // VIOLATION SRP/DIP: Notifikasi terikat kuat dengan sistem order
         println("Email terkirim: Pesanan $itemName Anda telah dikonfirmasi!")
+    }
+}
+*/
+
+import java.io.File
+
+// 1. Abstraksi untuk Data Layer (DIP & SRP)
+interface OrderRepository {
+    fun saveOrder(itemName: String, finalPrice: Double, customerType: String)
+}
+
+// Implementasi repository menggunakan CSV File (Dengan penanganan aman)
+class CsvOrderRepository : OrderRepository {
+    private val file = File("orders.csv")
+
+    override fun saveOrder(itemName: String, finalPrice: Double, customerType: String) {
+        // Menggunakan standard writer penulisan file yang aman
+        file.bufferedWriter().use { writer ->
+            file.appendText("$itemName, $finalPrice, $customerType\n")
+        }
+    }
+}
+
+// 2. Abstraksi untuk Layanan Notifikasi (DIP & SRP)
+interface NotificationService {
+    fun sendNotification(itemName: String)
+}
+
+// Implementasi Notifikasi Email
+class EmailNotifier : NotificationService {
+    override fun sendNotification(itemName: String) {
+        println("Email terkirim: Pesanan $itemName Anda telah dikonfirmasi!")
+    }
+}
+
+// 3. Main Processor yang sudah di-inject abstraksi komponennya
+class SafeOrderProcessor(
+    private val repo: OrderRepository,
+    private val notifier: NotificationService
+) {
+    fun processOrder(itemName: String, basePrice: Double, customerType: String) {
+        val finalPrice = when (customerType) {
+            "REGULAR" -> basePrice
+            "VIP" -> basePrice * 0.90
+            else -> basePrice
+        }
+
+        println("Memproses pesanan $itemName seharga $finalPrice")
+        repo.saveOrder(itemName, finalPrice, customerType)
+        notifier.sendNotification(itemName)
     }
 }
